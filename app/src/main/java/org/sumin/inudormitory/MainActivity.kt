b30faaa82820e195f7b7c9f0f3de4e008f826b08
milk717 <robolindasoo@gmail.com>
2021-08-07 00:41:29 +0900

    툴바 디자인 끝, 근데 적용은 아직

--- app/src/main/java/org/sumin/inudormitory/MainActivity.kt
+++ app/src/main/java/org/sumin/inudormitory/MainActivity.kt
@@ -1,10 +1,13 @@
 package org.sumin.inudormitory
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import org.sumin.inudormitory.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,13 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val fragmentList = listOf(HomeFragment(),DemeritFragment(),BillFragment())       //프래그먼트 목록 생성
+        val fragmentList = listOf(HomeFragment(), DemeritFragment(), BillFragment())       //프래그먼트 목록 생성
         val adapter = FragmentAdapter(this)         //어댑터 생성
         adapter.fragmentList = fragmentList     //어댑터에 프래그먼트 목록 저장
         binding.viewpager.adapter = adapter     //뷰페이저 불러오고 어댑터 적용
 
-        val tabTitle = listOf<String>("디폴트","벌점","공공요금")      //디자인 끝나면 아이콘으로 바꾸기
-        TabLayoutMediator(binding.tabs,binding.viewpager){tab,position->
+        val tabTitle = listOf<String>("디폴트", "벌점", "공공요금")      //디자인 끝나면 아이콘으로 바꾸기
+        TabLayoutMediator(binding.tabs, binding.viewpager){ tab, position->
             tab.text=tabTitle[position]
         }.attach()
 
